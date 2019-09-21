5aa186e683193ef498bdb75a581481208db60b9c
Pitchaya98 <overtim2005@outlook.com>
2019-09-22 02:02:27 +0700

    game logic v.2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-// import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
+
+
+const word = ['Heineken','Federbrau','Singha']
+var item = word[Math.floor(Math.random()*word.length)];
 
 // const word = "Hello";
 class App extends Component {
+
+  newgame = () => {
+    window.location.reload(false);
+  }
   render() {
     return (
-      <div className = "App">
-        <h2 id="welcome">Let To Play Game</h2>
-        {
-        <WordCard value="hello WORLDS"/>
-        }
-        <h2 id="result"></h2>
+      <div className= "App">
+        <h1 className="welcom"> Let To Play Game</h1>
+
+        <WordCard value={item.toUpperCase()}/>
+        <h1 id="result"></h1>
+        <button id="newgame" className="button" onClick={this.newgame}>New Game</button>
       </div>
     );
 
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
 import CharacterCard from "./CharacterCard";
+import _ from 'lodash';
+
+const prepareStateFromWord = (given_word) => {
+     let word = given_word.toUpperCase()
+     let chars = _.shuffle(Array.from(word)) 
+     return { 
+         word, 
+         chars, 
+         attempt: 1, 
+         guess: [], 
+         completed: false 
+        } 
+    }
+
+
 
 export default class WordCard extends
 Component {
-    activationHandler = c => { console.log(`${c} has been activated.`) }
+    constructor(props){
+        super(props)
+        this.state = prepareStateFromWord(this.props.value)
+    }
+    activationHandler = (c) =>{
+        let guess = [...this.state.guess, c.toUpperCase()]
+        this.setState({guess})
+
+      
+        if(guess.length == this.state.chars.length){
+            console.log('Mistake Word: '+guess.join('').toString())
+            console.log('Correct Word: '+this.state.chars.join('').toString())
+            if(guess.join('').toString() == this.state.chars.join('').toString()){
+                this.setState({guess: [], complete: true})
+                document.getElementById('result').innerHTML = `Congratulations! Or Succees`
+               // document.getElementById('newgame').style.display = "inline-block";
+                //document.getElementById('answer').style.display = "none";
+                
+            }else{
+                this.setState({guess: [], attempt: this.state.attempt + 1})
+                document.getElementById('result').innerHTML = `Attempt Or Try Again: ${this.state.attempt} `
+                //document.getElementById('wordenter').innerHTML = `You click is Wrong Word: ${guess.join('').toString()} `
+            }
+        if(this.state.attempt == 3){
+            document.getElementById('result').innerHTML = `Game Over! So RestartGame`
+            //document.getElementById('wordenter').innerHTML = `Answer is: ${this.state.chars.join('').toString()}`
+            setTimeout(() => window.location.reload(false),3000)
+        }
+        }
+    }
     render() {
         return (
             <div>
